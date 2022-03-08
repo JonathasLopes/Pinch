50a56411b3eb4ae6bc8602da7d35d921235a7d6b
JonathasLopes <jon.athasmoreira@hotmail.com>
2022-03-07 23:59:39 -0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Jonathas Lopes on 07/03/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
